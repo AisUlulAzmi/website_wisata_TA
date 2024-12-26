1d733ed00c2fd168ceb1fda8f117040d1380d4e8
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2024-12-26 15:42:18 +0700

    CRUD Clients

---
+++ app/Filament/Resources/ClientsResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ClientsResource\Pages;
+use App\Filament\Resources\ClientsResource\RelationManagers;
+use App\Models\Clients;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ClientsResource extends Resource
+{
+    protected static ?string $model = Clients::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    protected static ?string $navigationGroup = 'Wisata';
+
+    protected static ?string $modelLabel = "Klien";
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->columns(1)
+            ->schema([
+                TextInput::make('name')
+                    ->label('Nama')
+                    ->required(),
+                TextInput::make('link')
+                    ->label('Link External')
+                    ->required(),
+                FileUpload::make('image')
+                    ->label('Gambar')
+                    ->image()
+                    ->required(),
+                Select::make('is_published')
+                    ->label('Dipublikasikan')
+                    ->options([
+                        0 => 'Tidak',
+                        1 => 'Ya',
+                    ])
+                    ->default(1)
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('image')
+                    ->label('Gambar'),
+                TextColumn::make('name')
+                    ->label('Nama')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListClients::route('/'),
+            'create' => Pages\CreateClients::route('/create'),
+            'edit' => Pages\EditClients::route('/{record}/edit'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             Destination::class,
+            Clients::class,
+            News::class,
             UsersSeeder::class,
         ]);
     }
