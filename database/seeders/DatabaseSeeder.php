72beffff38a5b56af6c75f717a6974b510cf2325
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2024-12-28 13:01:33 +0700

    Settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            Settings::class,
             Destination::class,
             Clients::class,
             News::class,
---
+++ database/seeders/Settings.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Settings extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $table = 'settings';
+        $json = database_path('seeders/json/settings.json');
+        $img = database_path('seeders/img/settings');
+
+        if (DB::table($table)->count() === 0 and file_exists($json)) {
+            $payload = json_decode(file_get_contents($json), true);
+            foreach ($payload as $data) {DB::table($table)->insert($data);}
+
+            // Copy all images to storage folder
+            if ($img and file_exists($img)) {
+                $destination = storage_path('app/public');
+                if (!file_exists($destination)) {mkdir($destination, 0777, true);}
+
+                $files = glob($img . '/*');
+                foreach ($files as $file) {
+                    $name = basename($file);
+                    copy($file, $destination . '/' . $name);
+                }
+            }
+        }
+    }
+}
